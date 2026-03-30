6f6a64118e75efbbea8a728d7e99ab3ecde6ea8b
RonnyCevallos <ronny.cevallos@est.ucacue.edu.ec>
2026-03-30 12:55:07 -0500

    Hola

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float sprintMultipler = 2.0f;
+
+
 
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal"); // flechas izquierda/derecha
         float moveZ = Input.GetAxis("Vertical");   // flechas arriba/abajo
+        float currentSpeed = speed;
 
-        Vector3 movement = new Vector3(moveX, 0f, moveZ).normalized;
+
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            currentSpeed *= sprintMultipler;
+        }
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
 
         transform.Translate(movement * speed * Time.deltaTime);
     }
